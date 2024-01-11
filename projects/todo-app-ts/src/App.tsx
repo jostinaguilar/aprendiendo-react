3cfd45301b03188e56d86306e45ad285a100f383
Jostin Aguilar <aguilarjostin2001@gmail.com>
2024-01-10 23:10:22 -0500

    add function remove todo

--- projects/todo-app-ts/src/App.tsx
+++ projects/todo-app-ts/src/App.tsx
@@ -1,9 +1,40 @@
-import './App.css'
+import { useState } from 'react'
+import { Todos } from './components/Todos'
 
-function App () {
+const mockTodos = [
+  {
+    id: '1',
+    title: 'todo 1',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    completed: false
+  }
+]
+
+function App (): JSX.Element {
+  const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove = (id: string): void => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      Todo App
+      <h1>Todo App</h1>
+      <div className='todoapp'>
+        <Todos
+          onRemoveTodo={handleRemove}
+          todos={todos}/>
+      </div>
     </>
   )
 }
---
+++ projects/todo-app-ts/src/components/Todo.tsx
@@ -0,0 +1,15 @@
+import { type Todo as TodoType } from '../types'
+
+interface Props extends TodoType {
+  onRemoveTodo: (id: string) => void
+}
+
+export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo }) => {
+  return (
+        <div className="view">
+            <input className="toggle" checked={completed} type="checkbox" onChange={() => {}}/>
+            <label htmlFor="">{title}</label>
+            <button className="destroy" onClick={() => { onRemoveTodo(id) }}></button>
+        </div>
+  )
+}
