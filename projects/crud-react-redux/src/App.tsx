24fc43186b53b05747187ce742b5443f7a0a24fb
Jostin Aguilar <aguilarjostin2001@gmail.com>
2024-01-13 23:41:57 -0500

    start project crud with redux

---
+++ projects/crud-react-redux/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { ListOfUsers } from "./ListOfUsers";
+
+function App() {
+	return (
+		<>
+			<h1>Crud with react and redux ⚛️</h1>
+			<ListOfUsers />
+		</>
+	);
+}
+
+export default App;
