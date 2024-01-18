2152f34c56f9bebfd2dde2e02af38b00e9ad9908
Jostin Aguilar <aguilarjostin2001@gmail.com>
2024-01-17 22:41:11 -0500

    fetch users random api

--- projects/react-test-55k/src/App.tsx
+++ projects/react-test-55k/src/App.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react'
+import { type Users } from './types.d'
 import './App.css'
 
 function App () {
+  const [users, setUsers] = useState<Users[]>([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=100')
+      .then(async res => await res.json())
+      .then(data => { setUsers(data.results) })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
   return (
     <>
       <div>
         <h1>React Test 55k</h1>
       </div>
-
+{JSON.stringify(users)}
     </>
   )
 }
